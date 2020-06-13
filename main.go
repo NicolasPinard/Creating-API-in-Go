071c685d03b58a747b42a45b2d54db1a18964d98
Nicolas Pinard <nicolas.pinard@ticksmith.com>
2020-06-13 17:48:59 -0400

    Added article persistence on creation and GET indiviual article endpoint

--- main.go
+++ main.go
@@ -1,46 +1,89 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // Article forms the most basic datatype of this API
 type Article struct {
-	Title   string `json:"Title`
+	Title   string `json:"Title"`
 	Desc    string `json:"Desc"`
 	Content string `json:"Content"`
+	URL     string `json:"URL"`
 }
 
 // Articles is an array of Article
 type Articles []Article
 
-var articles = Articles{
-	Article{Title: "Test Title", Desc: "Lorem ipsum", Content: "Hello World!"},
+// Datastore cleanly separates the concerns of reading and writing to the database
+// type Datastore interface {
+// 	Mutations
+// 	Queries
+// }
+
+// Client wraps a pool of Sqlite connections.
+type Client struct {
+	*sql.DB
 }
+
+var articlesCache = Articles{}
 
 func getAllArticles(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Endpoint hit: All Articles endpoint")
-	json.NewEncoder(w).Encode(articles)
+	json.NewEncoder(w).Encode(articlesCache)
+}
+
+func getArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+	fmt.Println("Endpoint hit: GET article endpoint with id " + key)
+	keyID, err := strconv.ParseInt(key, 10, 64)
+	if err != nil || keyID < 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error": "the article id given is not a positive integer"}`))
+	}
+
+	dbClient := NewDBClient("./articles.db")
+	defer dbClient.Close()
+	article, _ := dbClient.QueryArticle(keyID)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(article)
 }
 
 func createArticle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	err := r.ParseForm()
+	decoder := json.NewDecoder(r.Body)
+	var article Article
+	err := decoder.Decode(&article)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(`{"error": "body not parsed"}`))
 		return
 	}
 
-	articles = append(articles, Article{Title: r.FormValue("Title"), Desc: r.FormValue("Desc"), Content: r.FormValue("Content")})
+	client := NewDBClient("./articles.db")
+	defer client.Close()
+	articleID, err := client.InsertArticle(&article)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "unable to persist article"}`))
+		return
+	}
+	fmt.Printf("Created article %d\n", articleID)
+	articlesCache = append(articlesCache, article)
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(`{"success": "created"`))
+	w.Write([]byte(`{"success": "created article"}`))
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -53,5 +96,62 @@ func main() {
 	r.HandleFunc("/", homePage)
 	r.HandleFunc("/articles", getAllArticles).Methods(http.MethodGet)
 	r.HandleFunc("/articles", createArticle).Methods(http.MethodPost)
+	r.HandleFunc("/articles/{id}", getArticle).Methods(http.MethodGet)
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+// NewDBClient creates a Sqlite3 Client
+func NewDBClient(path string) *Client {
+	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		log.Fatalf("Error while connecting to sqlite3 database: %v", err)
+	}
+	return &Client{db}
+}
+
+// InsertArticle persists the article passed to the database client
+func (c *Client) InsertArticle(a *Article) (int64, error) {
+	statement, err := c.Prepare("CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY, title TEXT, desc TEXT, content TEXT, url TEXT)")
+	if err != nil {
+		log.Fatalf("Unable to create table: %v", err)
+		return -1, err
+	}
+	statement.Exec()
+	statement, _ = c.Prepare("INSERT INTO articles (title, desc, content, url) VALUES (?, ?, ?, ?)")
+	execResult, err := statement.Exec(a.Title, a.Desc, a.Content, a.URL)
+	if err != nil {
+		return -1, err
+	}
+	rows, _ := execResult.RowsAffected()
+	defer statement.Close()
+
+	fmt.Printf("Successfully created %d row(s).\n", rows)
+
+	return execResult.LastInsertId()
+}
+
+// QueryArticle retrieves an article from the database
+func (c *Client) QueryArticle(id int64) (*Article, error) {
+	row := c.QueryRow("SELECT name FROM sqlite_master WHERE type='table' AND name='articles'")
+	var tableName string
+	err := row.Scan(&tableName)
+	if err != nil {
+		log.Fatalln("Unable to find table 'articles'")
+		return nil, err
+	}
+
+	statement, _ := c.Prepare("SELECT * FROM articles WHERE id=$1")
+	articleRow := statement.QueryRow(id)
+	var articleID int64
+	var title string
+	var desc string
+	var content string
+	var url string
+	err = articleRow.Scan(&articleID, &title, &desc, &content, &url)
+	a := Article{Title: title, Desc: desc, Content: content, URL: url}
+	if err != nil {
+		log.Printf("Unable to find article with id %d\n", id)
+		return nil, err
+	}
+	return &a, nil
 }
